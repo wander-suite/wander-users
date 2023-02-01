467849cec1f31c001228fded15f4e411d677f540
Guo Hong Wu <jorm7012@gmail.com>
2023-02-01 18:22:17 -0500

    Added typescript configuration file, added boiler plate express code, install barebones dependencies for typescript & added dev command to npm

---
+++ service.ts
@@ -0,0 +1,7 @@
+import express, { Application } from 'express';
+
+const app: Application = express();
+const port: number = 8000;
+app.listen(port, () => {
+    console.log(`Starting service on port ${port}`);
+});
