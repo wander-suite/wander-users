f416343d8c8ce0185396ca893f24d5601f6d51da
chenjosephine <jchen1114@gmail.com>
2023-02-01 21:42:50 -0500

    setup files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
   root: true,
-  extends: 'airbnb-typescript/base',
-  plugins: ['import', 'prettier'],
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['import', '@typescript-eslint', 'prettier'],
+  parser: ['@typescript-eslint/parser', 'prettier/@typescript-eslint'],
   parserOptions: {
     project: './tsconfig.eslint.json',
   },
--- src/service.ts
+++ src/service.ts
@@ -1,7 +1,41 @@
-import express, { Application } from 'express';
+import express, { Request, Response, ErrorRequestHandler, Application } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
 
+const PORT = 3000;
 const app: Application = express();
-const port: number = 8000;
-app.listen(port, () => {
-  console.log(`Starting service on port ${port}`);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+if (process.env.NODE_ENV == 'production') {
+  app.use('/dist', express.static(path.join(__dirname, '../dist')));
+
+  app.get('/', (req: Request, res: Response) => {
+    return res.status(200).sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+}
+
+// catch-all route handler for any requests to an unknown route
+app.use((req: Request, res: Response) => res.status(404).send('Error: Page not found'));
+
+// express error handler
+
+const errorRequestHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+};
+
+app.use(errorRequestHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
 });
